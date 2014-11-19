7a62445748a0ca7b56a6f9d1306820c25ba29944
kalo-glb <kalo.glb@gmail.com>
2014-11-19 16:16:59 +0200

    first motor test

--- Izhod_ot_labirint.ino
+++ Izhod_ot_labirint.ino
@@ -1,12 +1,43 @@
+enum MotorData
+{
+  Back,
+  Speed,
+  Forward
+};
+
 int l_motor[] = {4, 6, 7};
 int r_motor[] = {8, 10, 12};
 
 void setup()
 {
-  pinmode
+  pinMode(l_motor[Back], OUTPUT);
+  pinMode(l_motor[Speed], OUTPUT);
+  pinMode(l_motor[Forward], OUTPUT);
+  pinMode(r_motor[Back], OUTPUT);
+  pinMode(r_motor[Speed], OUTPUT);
+  pinMode(r_motor[Forward], OUTPUT);
+  
+  analogWrite(l_motor[Speed], 200);
+  analogWrite(r_motor[Speed], 200);
+  
+  digitalWrite(l_motor[Back], LOW);
+  digitalWrite(l_motor[Forward], LOW);
+  digitalWrite(r_motor[Back], LOW);
+  digitalWrite(r_motor[Forward], LOW);
 }
 
 void loop()
 {
-  
+  digitalWrite(l_motor[Back], HIGH);
+  digitalWrite(r_motor[Back], HIGH);
+  delay(3000);
+  digitalWrite(l_motor[Back], LOW);
+  digitalWrite(r_motor[Back], LOW);
+  delay(300);
+  digitalWrite(l_motor[Forward], HIGH);
+  digitalWrite(r_motor[Forward], HIGH);
+  delay(3000);
+  digitalWrite(l_motor[Forward], LOW);
+  digitalWrite(r_motor[Forward], LOW);
+  delay(300);
 }
