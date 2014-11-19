1d6ba4c81e7737835b934d0d95fbdb2742445f70
kalo-glb <kalo.glb@gmail.com>
2014-11-19 18:42:44 +0200

    first commit for sensor test

--- Izhod_ot_labirint.ino
+++ Izhod_ot_labirint.ino
@@ -1,12 +1,22 @@
-int l_motor[] = {4, 6, 7};
-int r_motor[] = {8, 10, 12};
+int l_sen = A1;
+int r_sen = A4;
+
+int l_dist = 0;
+int r_dist = 0;
 
 void setup()
 {
-  pinmode
-}
+  pinMode(l_sen, INPUT);
+  pinMode(r_sen, INPUT);
+  Serial.begin(9600);
+} 
 
 void loop()
 {
-  
+  l_dist = analogRead(l_sen);
+  r_dist = analogRead(r_sen);
+  Serial.print(l_dist);
+  Serial.print(" ");
+  Serial.println(r_dist);
+  delay(400);
 }
